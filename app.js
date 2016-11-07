2a5fb54e6406778cee73fac975f678e7cd8b428a
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-11-07 17:53:14 -0600

    More things

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 
 $(document).ready(function() {
-
+    var colorCount = 0;
 //gets the button working
   $('.color-button').on('click', function(){
     //calls the add block on a click
     addColorBlock($(this).data("color"));
     //calls the replace tracker on the click
     updateTracker($(this).data("color"));
-    console.log("working");
   });
 
   function addColorBlock(color) {
     //appends correct color cube to container
     $('.container').append('<div class="color-cube ' + color + '"></div>');
-    console.log("hello");
   }
 
   function updateTracker(color){
-    var colorCount = 0;
     colorCount++;
-    $('#'+ color).replaceWith('<p>Total ' + color + " :" + colorCount);
+    $('#'+ color).text('Total ' + color + " :" + colorCount);
 
   }
 
