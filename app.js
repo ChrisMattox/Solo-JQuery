c2b7692daa728f82cc7c9dd110bff84cf3517efd
ChrisMattox <metroplex02@gmail.com>
2016-11-08 07:59:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 
 $(document).ready(function() {
     var colorCount = 0;
-//gets the button working
   $('.color-button').on('click', function(){
-    //calls the add block on a click
     addBlockz($(this).data("color"));
-    //calls the replace tracker on the click
     updateTracker($(this).data("color"));
   });
 
   function addBlockz(color) {
-    //appends correct color cube to container
     $('.container').append('<div class="color-cube ' + color + '"></div>');
   }
 
   function updateTracker(color){
     colorCount++;
     $('#'+ color).text('Total ' + color + " :" + colorCount);
-
   }
-
 });
