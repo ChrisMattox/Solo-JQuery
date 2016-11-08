41c341db394c4aee71a62e7fde8e6852ef181044
ChrisMattox <metroplex02@gmail.com>
2016-11-08 07:58:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
 //gets the button working
   $('.color-button').on('click', function(){
     //calls the add block on a click
-    addColorBlock($(this).data("color"));
+    addBlockz($(this).data("color"));
     //calls the replace tracker on the click
     updateTracker($(this).data("color"));
   });
 
-  function addColorBlock(color) {
+  function addBlockz(color) {
     //appends correct color cube to container
     $('.container').append('<div class="color-cube ' + color + '"></div>');
   }
